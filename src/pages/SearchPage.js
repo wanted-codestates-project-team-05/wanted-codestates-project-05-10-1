16a31fda4540f51f9ec52659820be46626c6d2ea
kyu <2066apple@naver.com>
2022-03-11 14:08:58 +0900

    Fix: title 리팩토링 / Delte: 안쓰는 import문 삭제

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -22,6 +22,7 @@ function SearchPage() {
 	const { data } = useGetSearchWordQuery(debounceInputValue, 
 		{ skip: debounceInputValue === '' ? true : false }
 	);
+		
 	const coments = <>국내 모든 임상시험 검색하고<br/>온라인으로 참여하기</>;
 
 	const handleKeyPress = (e) => {
