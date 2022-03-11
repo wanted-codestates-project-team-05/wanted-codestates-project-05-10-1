8361f21188ac35ad2bd36dc37d0d6011ed2ed95e
kyu <2066apple@naver.com>
2022-03-11 14:08:38 +0900

    Fix: 검색어 빈값일때 dataList나오는문제 해결

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -22,7 +22,6 @@ function SearchPage() {
 	const { data } = useGetSearchWordQuery(debounceInputValue, 
 		{ skip: debounceInputValue === '' ? true : false }
 	);
-		
 	const coments = <>국내 모든 임상시험 검색하고<br/>온라인으로 참여하기</>;
 
 	const handleKeyPress = (e) => {
