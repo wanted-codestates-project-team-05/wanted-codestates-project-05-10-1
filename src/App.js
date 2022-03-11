022c1449a9e0cb37ad7f2900eccb2d83c5dbc9e2
kyu <2066apple@naver.com>
2022-03-11 11:12:46 +0900

    Delete: test page 삭제

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import SearchPage from './pages/SearchPage';
-import TestPage from './pages/TestPage';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <div className="App">
       <Routes>
         <Route path={'/'} element={<SearchPage/>}/>
-        <Route path={'/test'} element={<TestPage/>}/>
       </Routes>
     </div>
   );
